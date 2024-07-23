b7bbe9ed75c8e9f6ae6645890296d42720c16baa
Ivo Tsakanski <ivcho.ts@gmail.com>
2024-07-23 14:08:30 +0300

    listview

--- lib/widgets/forecast_list.dart
+++ lib/widgets/forecast_list.dart
@@ -19,22 +19,50 @@ class ForecastList extends StatelessWidget {
             margin: const EdgeInsets.all(8),
             padding: const EdgeInsets.all(8),
             decoration: BoxDecoration(
-              color: Colors.lightBlueAccent,
-              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Colors.white,
+                width: 3.5,
+              ),
+              borderRadius: BorderRadius.circular(15),
+              gradient: const LinearGradient(colors: [
+                Color.fromARGB(255, 2, 76, 67),
+                Color.fromARGB(255, 26, 149, 7),
+                Color.fromARGB(255, 4, 69, 61),
+
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '${forecast[index].date?.weekday}',
-                  style: const TextStyle(color: Colors.white),
-                ),
-                Text(
-                  '${forecast[index].temperature?.celsius?.toStringAsFixed(1)} °C',
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
+
+
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${forecast[index].date?.weekday}',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18,
+                    ),
+                  ),
+
+                  Text(
+                    '${forecast[index].temperature?.celsius?.toStringAsFixed(0)} °C',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Icon(
+                    Icons.wb_sunny,
+                    size: 35,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+
           );
         },
       ),
